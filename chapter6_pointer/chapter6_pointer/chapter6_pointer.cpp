df597c30ad9e1003ec27fe35b1b4b512257d1a55
Geek song <503455810@qq.com>
2017-12-12 16:56:48 +0800

    6.12a quiz

--- chapter6_pointer/chapter6_pointer/chapter6_pointer.cpp
+++ chapter6_pointer/chapter6_pointer/chapter6_pointer.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+bool searchName(std::string name,std::string (&array)[8])
+{
+	for (const auto &ref : array)
+		if (name == ref)
+			return true;
+		else
+			return false;
+
+}
 
 int main()
 {
+	std::string array[] = { "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg",  "Holly" };
+	std::cout << "please input one name \n";
+	std::string name; 
+	std::getline(std::cin, name);
+	
+	bool isFind = searchName(name, array);
+	if (isFind)
+		std::cout << "we find " << name << "\n";
+	else
+		std::cout << "we did not find " << name << "\n";
+
     return 0;
 }
 
