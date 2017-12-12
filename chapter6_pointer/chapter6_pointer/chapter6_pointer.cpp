976e99e922bed06763a76f89a13bac6948dfb8e9
Geek song <503455810@qq.com>
2017-12-12 15:27:05 +0800

    bubble

--- chapter6_pointer/chapter6_pointer/chapter6_pointer.cpp
+++ chapter6_pointer/chapter6_pointer/chapter6_pointer.cpp
@@ -2,10 +2,38 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+
 
 
 int main()
 {
-    return 0;
+	std::cout << "How many names would you like to enter?\n";
+	int numberOfName;
+	std::cin >> numberOfName;
+	std::cin.ignore(32767, '\n');
+	std::string *ptr = new std::string[numberOfName];
+	//get input
+	for (int i = 0; i < numberOfName; ++i)
+	{
+		std::cout << "Enter name #" << i << ": ";
+		std::getline(std::cin, *(ptr+i));
+	}
+	//sort names
+	for (int end = numberOfName-1; end >=1; --end)
+	{
+		for (int current = 0; current < end; ++current)
+		{
+			if (*(ptr + current) > *(ptr + current + 1))
+				std::swap(*(ptr + current), *(ptr + current + 1));
+		}
+	}
+	//output
+	std::cout << "Here is your sorted list:\n";
+	for (int i = 0; i < numberOfName; ++i)
+		std::cout << *(ptr + i)<<"\n";
+
+
 }
 
